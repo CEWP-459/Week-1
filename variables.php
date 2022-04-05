eddc27426cef249087d0d6a658033a51a134c28a
Kritika Sharma <35176749+KritikaSharmaKS@users.noreply.github.com>
2022-04-04 21:31:27 -0400

    types of variables

--- variables.php
+++ variables.php
@@ -4,3 +4,14 @@
 $message = "Hello World!";
 echo $message;
 
+#Types of Variables
+$name = "Kritika Sharma";
+$age = 45;
+$salary = 45.55;
+$is_admin = true;
+$adminPassword = null;
+
+var_dump($name);
+var_dump($age);
+var_dump($salary);
+var_dump($is_admin, $adminPassword);
